b466e57ce0e348df48bcfddd360fff2c3ed0893a
Jackie <zhenghongjie@qq.com>
2022-01-19 13:28:11 +0800

    init Add project files.

---
+++ main.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace StartWeChat
+{
+    partial class main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.softPathLb = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nud = new System.Windows.Forms.NumericUpDown();
+            this.nubLb = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbPath.Location = new System.Drawing.Point(134, 61);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(553, 30);
+            this.tbPath.TabIndex = 0;
+            // 
+            // softPathLb
+            // 
+            this.softPathLb.AutoSize = true;
+            this.softPathLb.Location = new System.Drawing.Point(46, 64);
+            this.softPathLb.Name = "softPathLb";
+            this.softPathLb.Size = new System.Drawing.Size(82, 24);
+            this.softPathLb.TabIndex = 1;
+            this.softPathLb.Text = "微信地址";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(321, 217);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 34);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "启动";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // nud
+            // 
+            this.nud.AutoSize = true;
+            this.nud.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nud.Location = new System.Drawing.Point(134, 122);
+            this.nud.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud.Name = "nud";
+            this.nud.Size = new System.Drawing.Size(180, 30);
+            this.nud.TabIndex = 3;
+            this.nud.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // nubLb
+            // 
+            this.nubLb.AutoSize = true;
+            this.nubLb.Location = new System.Drawing.Point(46, 124);
+            this.nubLb.Name = "nubLb";
+            this.nubLb.Size = new System.Drawing.Size(82, 24);
+            this.nubLb.TabIndex = 1;
+            this.nubLb.Text = "启动个数";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 354);
+            this.Controls.Add(this.nud);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.nubLb);
+            this.Controls.Add(this.softPathLb);
+            this.Controls.Add(this.tbPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StartWeChat";
+            this.Load += new System.EventHandler(this.main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label softPathLb;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nud;
+        private System.Windows.Forms.Label nubLb;
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StartWeChat
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+        //D:\Program Files(x86)\Tencent\WeChat\WeChat.exe
+        public static bool TryGetSoftwarePath(string softName, out string path)
+        {
+            string strPathResult = string.Empty;
+            string strKeyName = "";     //"(Default)" key, which contains the intalled path
+            object objResult = null;
+
+            Microsoft.Win32.RegistryValueKind regValueKind;
+            Microsoft.Win32.RegistryKey regKey = null;
+            Microsoft.Win32.RegistryKey regSubKey = null;
+
+            try
+            {
+                //Read the key
+                regKey = Microsoft.Win32.Registry.LocalMachine;
+                regSubKey = regKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\" + softName.ToString() + ".exe", false);
+
+                //Read the path
+                objResult = regSubKey.GetValue(strKeyName);
+                regValueKind = regSubKey.GetValueKind(strKeyName);
+
+                //Set the path
+                if (regValueKind == Microsoft.Win32.RegistryValueKind.String)
+                {
+                    strPathResult = objResult.ToString();
+                }
+            }
+            catch (System.Security.SecurityException ex)
+            {
+                throw new System.Security.SecurityException("You have no right to read the registry!", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Reading registry error!", ex);
+            }
+            finally
+            {
+
+                if (regKey != null)
+                {
+                    regKey.Close();
+                    regKey = null;
+                }
+
+                if (regSubKey != null)
+                {
+                    regSubKey.Close();
+                    regSubKey = null;
+                }
+            }
+
+            if (strPathResult != string.Empty)
+            {
+                //Found
+                path = strPathResult;
+                return true;
+            }
+            else
+            {
+                //Not found
+                path = null;
+                return false;
+            }
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string softName = "WeChat.exe";
+                string strKeyName = "InstallPath";
+                string softPath = @"Software\Tencent\WeChat\";
+                RegistryKey regKey = Registry.CurrentUser;
+                RegistryKey regSubKey = regKey.OpenSubKey(softPath, false);
+
+                object objResult = regSubKey.GetValue(strKeyName);
+                RegistryValueKind regValueKind = regSubKey.GetValueKind(strKeyName);
+                if (regValueKind == Microsoft.Win32.RegistryValueKind.String)
+                {
+                    tbPath.Text = Path.Combine(objResult.ToString(), softName);
+                }
+
+                
+            }
+            catch
+            {
+                tbPath.Text = "获取失败，请手动输入";
+            }
+
+        }
+
+        public static void CloseApp(string softName)
+        {
+            Process[] pProcess;
+            pProcess = Process.GetProcesses();
+            for (int i = 1; i <= pProcess.Length - 1; i++)
+            {
+                if (pProcess[i].ProcessName == softName)
+                {
+                    pProcess[i].Kill();
+                    break;
+                }
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (nud.Value > 0)
+            {
+                CloseApp("WeChat");
+                for (int i = 0; i < nud.Value; i++)
+                {
+                    Process.Start(tbPath.Text);
+                }
+
+            }
+        }
+    }
+}
