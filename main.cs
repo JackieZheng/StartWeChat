97e1f1d7e86e4d63a7a5ac6743bef51d238b7cde
Jackie <zhenghongjie@qq.com>
2022-01-19 18:07:32 +0800

    update

--- main.cs
+++ main.cs
@@ -20,69 +20,7 @@ namespace StartWeChat
             InitializeComponent();
         }
         //D:\Program Files(x86)\Tencent\WeChat\WeChat.exe
-        public static bool TryGetSoftwarePath(string softName, out string path)
-        {
-            string strPathResult = string.Empty;
-            string strKeyName = "";     //"(Default)" key, which contains the intalled path
-            object objResult = null;
-
-            Microsoft.Win32.RegistryValueKind regValueKind;
-            Microsoft.Win32.RegistryKey regKey = null;
-            Microsoft.Win32.RegistryKey regSubKey = null;
-
-            try
-            {
-                //Read the key
-                regKey = Microsoft.Win32.Registry.LocalMachine;
-                regSubKey = regKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\" + softName.ToString() + ".exe", false);
-
-                //Read the path
-                objResult = regSubKey.GetValue(strKeyName);
-                regValueKind = regSubKey.GetValueKind(strKeyName);
-
-                //Set the path
-                if (regValueKind == Microsoft.Win32.RegistryValueKind.String)
-                {
-                    strPathResult = objResult.ToString();
-                }
-            }
-            catch (System.Security.SecurityException ex)
-            {
-                throw new System.Security.SecurityException("You have no right to read the registry!", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Reading registry error!", ex);
-            }
-            finally
-            {
-
-                if (regKey != null)
-                {
-                    regKey.Close();
-                    regKey = null;
-                }
-
-                if (regSubKey != null)
-                {
-                    regSubKey.Close();
-                    regSubKey = null;
-                }
-            }
-
-            if (strPathResult != string.Empty)
-            {
-                //Found
-                path = strPathResult;
-                return true;
-            }
-            else
-            {
-                //Not found
-                path = null;
-                return false;
-            }
-        }
+        
 
         private void main_Load(object sender, EventArgs e)
         {
